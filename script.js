06bbceb59cfa5866d63e9275299a0bc26db2d964
venkatesh-khatake <venkateshkhatake8805@gmail.com>
2024-12-04 20:11:50 +0530

    Updated

--- script.js
+++ script.js
@@ -67,15 +67,48 @@ const postMethods = () =>{
             <div class="img-box">
             <img src="${postData.image}" alt="${postData.name}">
               </div>
-              <button> <i class="fa-solid fa-cart-shopping"></i>Add to Cart</button>
+              <button class = "add-to-cart"> <i class="fa-solid fa-cart-shopping"></i>Add to Cart</button>
 
               <div class="details">
                 <h5>${postData.category}</h5>
                 <h4>${postData.name}</h4>
-                <h6>$,${postData.price}</h6>
+                <h6>$ ${postData.price.toFixed(2)}</h6>
               </div>
         ` 
         postContainer.appendChild(postElement);
     })
 }
 postMethods();
+
+
+// ========================================================
+
+document.querySelector("#card-box").addEventListener("click",(e) =>{
+    if(e.target.classList.contains("add-to-cart")){
+        const card = e.target.closest(".cards");
+
+        const name = card.querySelector("h4").textContent;
+        const price = card.querySelector("h6").textContent;
+
+        const cartItem = document.createElement("div");
+        cartItem.classList.add("cart-items");
+        cartItem.innerHTML = `
+            <div class = "items">
+                <h5>${name}</h5>
+                <h6>${price}</h6>
+            </div>
+            <i class="fa-solid fa-xmark remove-item"></i>
+        `;
+        document.querySelector(".cart-details").appendChild(cartItem);
+    }
+});
+
+document.querySelector(".cart-details").addEventListener("click", (e) => {
+        if (e.target.classList.contains("remove-item")) {
+            e.target.closest(".cart-items").remove();
+        }
+     });
+
+    //  ==========================================================
+
+
